6e8d169eb9cc3e94529544944da599c0dba5d49a
Anne Lin <annelin@Annes-MacBook-Pro-2.local>
2016-04-27 22:24:07 -0700

    y a s

---
+++ assets/js/script.js
@@ -0,0 +1,63 @@
+$(document).ready(function(){
+//worked side	
+	$("#worked").click(function(){
+		$(".title").fadeOut();
+		$(".sc_right").addClass("sc_old_right");
+		$(".sc_left").addClass("sc_new_left");
+		$(".home").addClass("home_left");
+		$(".work_container1").css("display", "block");
+
+
+	});
+	$(".sc_right").hover(function(){
+		if($(".sc_right").hasClass("sc_old_right")){
+			$(".sc_left").css("left","-50px");
+			$(".home").css("left","-50px");
+
+		}
+	}, function(){
+		$(".sc_left").css("left","0px");
+		$(".home").css("left","0px");
+
+	});
+
+	$(".sc_right").click(function(){
+		if($(".sc_right").hasClass("sc_old_right")){
+			$(".title").fadeIn();
+			$(".sc_right").removeClass("sc_old_right");
+			$(".sc_left").removeClass("sc_new_left");
+			$(".home").removeClass("home_left");
+			$(".work_container1").css("display", "none");
+
+		}
+	});
+//played side
+	$("#played").click(function(){
+		$(".title").fadeOut();
+		$(".sc_right").addClass("sc_new_right");
+		$(".sc_left").addClass("sc_old_left");
+		$(".home").addClass("home_right");
+
+	});
+
+	$(".sc_left").hover(function(){
+		if($(".sc_left").hasClass("sc_old_left")){
+			$(".sc_right").css("right","-50px");
+			$(".home").css("right","-50px");
+		}
+	}, function(){
+		$(".sc_right").css("right","0px");
+		$(".home").css("right","0px");
+
+	});
+
+	$(".sc_left").click(function(){
+		if($(".sc_left").hasClass("sc_old_left")){
+			$(".title").fadeIn();
+			$(".sc_left").removeClass("sc_old_left");
+			$(".sc_right").removeClass("sc_new_right");
+			$(".home").removeClass("home_right");
+		}
+	});
+	
+});
